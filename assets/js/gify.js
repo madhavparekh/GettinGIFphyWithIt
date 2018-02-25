61a4e86ec2e671f0a3904457bd0daf8a13511901
madhavparekh <madhavparekh@gmail.com>
2018-02-25 12:16:49 -0800

    displays GIFs for new topic when clicked on submit btn and adds btn

--- assets/js/gify.js
+++ assets/js/gify.js
@@ -26,43 +26,39 @@ $(document).ready(function () {
   });
 
   $('#disp-btn').on('click', 'button', function (r) {
-    console.log(r);
+    // console.log(r);
     apiTerms.q = r.target.innerHTML.trim();
-    console.log(apiTerms.q);
+    // console.log(apiTerms.q);
     var queryURL = apiURL + $.param(apiTerms);
+
+    deployGIFs(queryURL);
+
+  });
+
+  function deployGIFs(queryURL) {
+    $('#dispGIFs').empty();
 
     $.ajax({
       type: "GET",
       url: queryURL,
       data: "data",
       success: function (r) {
-        deployGIFs(r.data);
+        r.data.forEach(e => {
+          var titleRating = `<h6>${e.title} - Rating - ${e.rating}</h6>`;
+          var div = $('<div>').html(titleRating).addClass('m-2');
+          var gif = $('<img>').attr('src', e.images.original_still.url)
+            .attr('data-still', e.images.original_still.url)
+            .attr('data-animate', e.images.original.url)
+            .attr('data-state', 'still').addClass('gif');
+
+          div.append(gif);
+          $('#dispGIFs').append(div);
+        });
       }
-    });
-  });
-
-  function deployGIFs(r) {
-    $('#dispGIFs').empty();
-
-    r.forEach(e => {
-
-      var titleRating = `<h6>${e.title} - Rating - ${e.rating}</h6>`;
-
-      var div = $('<div>').html(titleRating).addClass('m-2');
-
-      var gif = $('<img>').attr('src', e.images.original_still.url)
-        .attr('data-still', e.images.original_still.url)
-        .attr('data-animate', e.images.original.url)
-        .attr('data-state', 'still').addClass('gif');
-
-      div.append(gif);
-      $('#dispGIFs').append(div);
     });
   }
 
   $('body').on('click', '.gif', function (r) {
-
-    console.log(r);
 
     var state = $(r.target).attr('data-state');
 
@@ -79,10 +75,14 @@ $(document).ready(function () {
   });
 
   $('#submitTpk').on('click', function () {
-    console.log($('#userTpk').val());
-    addBtn($('#userTpk').val());
+    // console.log($('#userTpk').val());
+    var usrTpk = $('#userTpk').val().trim();
+    addBtn(usrTpk);
     $('#userTpk').val('');
     event.preventDefault();
+    apiTerms.q = usrTpk;
+    var queryURL = apiURL + $.param(apiTerms);
+    deployGIFs(queryURL);
   });
 
   function addBtn(element) {
@@ -93,6 +93,4 @@ $(document).ready(function () {
     console.log(tpkBtn);
     $("#disp-btn").append(tpkBtn);
   }
-
-
 });
